4370f593fd26090541a598a3ecd6ed1cc9c0f0f8
Fallen-dev <rishav.m.19.02@gmail.com>
2022-08-21 22:52:45 +0530

    Update Person.java - update compare()

--- java/day 3/Person.java
+++ java/day 3/Person.java
@@ -1,4 +1,4 @@
-public class Person {
+class Person {
     String name;
     int age;
 
@@ -7,21 +7,19 @@ public class Person {
         this.name = name;
     }
 
-    static Person compare(Person p1, Person p2) {
+    void compare(Person p1, Person p2) {
         if (p1.age > p2.age)
-            System.out.printf("%s is senior than %s\n", p1.name, p2.name);
+            System.out.printf("%s (%d y/o) is senior than %s\n", p1.name, p1.age, p2.name);
         else if (p1.age < p2.age)
-            System.out.printf("%s is senior than %s\n", p2.name, p1.name);
+            System.out.printf("%s (%d y/o) is senior than %s\n", p2.name, p2.age, p1.name);
         else
             System.out.printf("%s and %s are at the same age\n", p1.name, p2.name);
-        return null;
     }
 
     public static void main(String[] args) {
         Person p1 = new Person(32, "John");
         Person p2 = new Person(54, "Alex");
 
-        Person compare = compare(p1, p2);
-        System.out.println(compare);
+        p1.compare(p1, p2);
     }
 }
