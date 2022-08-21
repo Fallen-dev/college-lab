619c550e878c27c0a6d7611227e81bd15bc2734c
Fallen-dev <rishav.m.19.02@gmail.com>
2022-08-21 22:30:56 +0530

    update Box.java - update compareVolume()

--- java/day 3/Box.java
+++ java/day 3/Box.java
@@ -18,21 +18,25 @@ class Box {
     }
 
     void compareVolume(Box b1, Box b2) {
-        if (b1.volume() > b2.volume())
+        if (b1.volume() > b2.volume()) {
             System.out.println("Box 1 has greater volume");
-        else if (b1.volume() < b2.volume())
+            System.out.println("Box 1 details ->");
+            System.out.printf("Width = %d\nHeight = %d\nDepth = %d\n", b1.width, b1.height, b1.depth);
+        }
+        else if (b1.volume() < b2.volume()) {
             System.out.println("Box 2 has greater volume");
-        else
-            System.out.println("Box 1 and Box 2 has equal volume");
+            System.out.println("Box 2 details ->");
+            System.out.printf("Width = %d\nHeight = %d\nDepth = %d\n", b2.width, b2.height, b2.depth);
+        }
+        else System.out.println("Both Box 1 and Box 2 has equal volume");
     }
 
     public static void main(String[] args) {
         Box b1 = new Box(4, 4, 2);
         Box b2 = new Box(5, 5, 3);
-        Box b3 = new Box(6, 6, 5);
 
         b1.display("Box 1 volume = ");
         b2.display("Box 2 volume = ");
-        b3.display("Box 3 volume = ");
+        b1.compareVolume(b1, b2);
     }
 }
